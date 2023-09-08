6da449b80040d502b06233926aeccbc9e414fb87
Dohou Daniel Favour <dohoudanielfavour@gmail.com>
2023-09-08 22:04:47 +0000

    Update file with my credentials.

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Dohou Daniel Favour",
+  title: "Software Engineer",
+  email: "dohoudanielfavour@gmail.com",
+  gitHub: "dohoudaniel",
+  instagram: "dohoudanielfavour",
+  linkedIn: "Dohou Daniel Favour",
+  medium: "Dohou Daniel Favour",
+  twitter: "DohouDaniel10",
+  youTube: "Daniel Dohou",
 };
 
 const primaryColor = "#4E567E";
